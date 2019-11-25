bf702e26bc17b0ab30a81e89b3ef441f16ed227b
NiamhAndrews <neand2006@gmail.net>
2019-11-25 10:50:10 +0000

    Started language conversion

--- Practical5-Matrix-And-Vectors/Vector3.cpp
+++ Practical5-Matrix-And-Vectors/Vector3.cpp
@@ -1,6 +1,8 @@
-namespace cube
-{
-	class Vector3
+#include "Vector3.h"
+
+
+
+class Vector3
 	{
 		// The class has three variables x, y and z 
 		double x;
@@ -12,60 +14,60 @@ namespace cube
 			get { return x; }
 			set { x = value; }
 		}
-			double Y
-			{
-				get { return y; }
-				set { y = value; }
+		double Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+		double Z
+		{
+			get { return z; }
+			set { z = value; }
+		}
+
+		// Constructor 1
+		public Vector3()
+		{
+			x = 0.0f;
+			y = 0.0f;
+			z = 0.0f;
+		}
+
+		// Constructor 2
+		public Vector3(double x1, double y1, double z1)
+		{ // To allow other values for X, Y and Z to be declared
+			x = x1;
+			y = y1;
+			z = z1;
+		}
+		// Constructor 3
+		public Vector3(Vector3 V)
+		{  // To allow other values for X, Y and Z to be declared
+			x = V.x;
+			y = V.y;
+			z = V.z;
+		}
+
+		public double Length()
+		{  // A method to return the length of the vector
+			return (double)Math.Sqrt(x * x + y * y + z * z);
+		}
+		public double LengthSquared()
+		{  // A method to return the length squared of the vector
+			return (x * x + y * y + z * z);
+		}
+
+		public void Normalise()
+		{  // A method to reduce the length of the vector to 1.0 
+			// keeping the direction the same
+			if (Length() > 0.0)
+			{  // Check for divide by zero
+				double magnit = Length();
+				x /= magnit;
+				y /= magnit;
+				z /= magnit;
 			}
-				 double Z
-				{
-					get { return z; }
-					set { z = value; }
-				}
-
-					// Constructor 1
-						public Vector3()
-					{
-						x = 0.0f;
-						y = 0.0f;
-						z = 0.0f;
-					}
-
-					// Constructor 2
-					public Vector3(double x1, double y1, double z1)
-					{ // To allow other values for X, Y and Z to be declared
-						x = x1;
-						y = y1;
-						z = z1;
-					}
-					// Constructor 3
-					public Vector3(Vector3 V)
-					{  // To allow other values for X, Y and Z to be declared
-						x = V.x;
-						y = V.y;
-						z = V.z;
-					}
-
-					public double Length()
-					{  // A method to return the length of the vector
-						return (double)Math.Sqrt(x * x + y * y + z * z);
-					}
-					public double LengthSquared()
-					{  // A method to return the length squared of the vector
-						return (x * x + y * y + z * z);
-					}
-
-					public void Normalise()
-					{  // A method to reduce the length of the vector to 1.0 
-						// keeping the direction the same
-						if (Length() > 0.0)
-						{  // Check for divide by zero
-							double magnit = Length();
-							x /= magnit;
-							y /= magnit;
-							z /= magnit;
-						}
-					}
+		}
 
 
 
@@ -120,4 +122,3 @@ namespace cube
 					}
 
 	}
-}
--- Practical5-Matrix-And-Vectors/Vector3.h
+++ Practical5-Matrix-And-Vectors/Vector3.h
@@ -1,1 +1,81 @@
 #pragma once
+#include <iostream>
+#include <string.h>
+#include <math.h>
+
+class Vector3
+{
+public:
+	// The class has three variables x, y and z 
+	double x;
+	double y;
+	double z;
+
+
+	
+	double X
+	{
+		get { return x; }
+		set { x = value; }
+	}
+	double Y
+	{
+		get { return y; }
+		set { y = value; }
+	}
+	double Z
+	{
+		get { return z; }
+		set { z = value; }
+	}
+	
+
+
+	// Constructor 1
+	Vector3();
+
+	// Constructor 2
+	Vector3(double x1, double y1, double z1);
+
+	// Constructor 3
+	Vector3(Vector3 V);
+
+
+
+	Vector3 operator +(Vector3 V1, Vector3 V2);
+
+
+	Vector3 operator -(Vector3 V1, Vector3 V2);
+
+
+	Vector3 operator -(Vector3 V);
+
+	Vector3 operator *(Vector3 V1, Vector3 V2);
+
+	Vector3 operator *(double k, Vector3 V1);
+
+	Vector3 operator *(float k, Vector3 V1);
+
+	Vector3 operator *(int k, Vector3 V1);
+
+
+	Vector3 operator ^(Vector3 V1, Vector3 V2);
+
+
+
+
+
+	double Length();
+
+	double LengthSquared();
+
+	void Normalise();
+
+
+
+
+
+
+	std::string ToString();
+	
+}
